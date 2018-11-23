d4b1a46073e2d2a75c9bbf5f9186651c1d518bca
José Romildo Mendes Júnior <jrmmendes@outlook.com>
2018-11-23 12:39:05 -0300

    Adicionado model para serviços

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Servico(models.Model):
+    nome = models.CharField('Nome do serviço', max_length=100)
+    descricao = models.TextField('Descrição do Serviço')
+    imagem = models.ImageField(upload_to = 'static/img/servicos/', default = 'static/img/servicos/default.png')
+
+    def __str__(self):
+        return self.nome
--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView 
+from core.models import *
 
 # Create your views here.
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+
+        return context

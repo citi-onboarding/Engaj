7d19b9ab045eea0fecbb9c57249161bcfe8777b5
José Romildo Mendes Júnior <jrmmendes@outlook.com>
2018-11-26 10:47:38 -0300

    Adicionados arquivos básicos da seção sobre

--- core/models.py
+++ core/models.py
@@ -5,6 +5,30 @@ class Servico(models.Model):
     nome = models.CharField('Nome do serviço', max_length=100)
     descricao = models.TextField('Descrição do Serviço')
     imagem = models.ImageField(upload_to = 'servicos/', default = 'servicos/default.jpg')
+    
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
 
     def __str__(self):
         return self.nome
+
+# Ver como tornar único  
+class Sobre(models.Model):
+    descricao = models.TextField('Descrição da Engaj')
+    imagem = models.ImageField(upload_to = 'sobre/', default = 'sobre/default.jpg')
+    visao = models.TextField('Visão da Engaj')
+    missao = models.TextField('Missao da Engaj')
+    
+    def __str__(self):
+        return "Sobre a Engaj"
+
+class Valor(models.Model):
+    descricao = models.TextField('Valor da Engaj')
+    
+    class Meta:
+        verbose_name = 'Valor'   
+        verbose_name_plural = 'Valores'   
+
+    def __str__(self):
+        return self.descricao

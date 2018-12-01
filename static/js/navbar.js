37900f803b51ad3ee76512383a02ebda45a1f46e
Ana Beatriz <abcr@cin.ufpe.br>
2018-11-30 23:21:17 -0300

    Alterando background da navbar

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,6 +1,6 @@
 function corverde() {
-    $('.navbartotal').css('background-color','#FFFFFF');
-    $('.navbartotal').css('transition-delay','0s');
+    $('.navbar').css('background-color','#FFFFFF');
+    $('.navbar').css('transition-delay','0s');
         $('.navbarlogo').css('background-image','url(static/img/engaj-verde-alt.svg)');
         $('.link a').css('color','rgb(8,137,98)');
         $('.icon-facebook-alt').css('color','rgb(8,137,98)');
@@ -9,41 +9,51 @@ function corverde() {
 }
 
 function cortransparente() {
-    $('.navbartotal').css('transition','0.5s ease-out');
-    $('.navbartotal').css('transition-delay','0.25s');
-    $('.navbartotal').css('background-color','');
+    $('.navbar').css('transition','0.5s ease-out');
+    $('.navbar').css('transition-delay','0.25s');
+    $('.navbar').css('background-color','');
         $('.navbarlogo').css('background-image','url(static/img/engaj-branco-alt.svg)');
         $('.link a').css('color','#FFFFFF');
+        $('link a').css('border-bottom','0.2rem solid #FFFFFF');
         $('.icon-facebook-alt').css('color','#FFFFFF');
         $('.icon-instagram-alt').css('color','#FFFFFF');
         $('.icon-menu').css('color','FFFFFF');
 }
-
 
 var c = 0;
 
 $('.icon-menu').on("click", function() {
     if(c===0){
         c=1;
-        $('.navbarmenusanduiche').animate({top:0, height:'toggle'},900);
+        $('.menusanduiche').animate({top:0, height:'toggle'},900);
         corverde();
     }else{
         c=0;
-        $('.navbarmenusanduiche').animate({height:'toggle'},250);
-        if($(window).scrollTop() < $(window).height()){
-            cortransparente();
-        }
+        $('.menusanduiche').animate({height:'toggle'},250);
+        if($(window).scrollTop() > ($(window).height()*0.1)){
+            corverde();
+        }else cortransparente();
     }
     
 })
 
 $(window).on("scroll", function() {
-    if($(window).scrollTop() < $(window).height()){
+    if($(window).scrollTop() < ($(window).height() * 0.1)) {
         if(c===1) corverde();
         else cortransparente();
     } else{
         corverde();
     }
 })
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+    
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top - ($(window).height() * 0.1)
+    }, 800);
+});
+
+
 
 

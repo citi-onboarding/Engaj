32a2e5f42d4ccbc3046cb514bcdb8f05c8ce62e7
José Romildo Mendes Júnior <jrmmendes@outlook.com>
2018-12-01 00:03:52 -0300

    Realizadas configurações do djang-storages com dropbox

--- engaj/settings/base.py
+++ engaj/settings/base.py
@@ -126,6 +126,9 @@ django_heroku.settings(locals())
 # Media files
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+DROPBOX_OAUTH2_TOKEN=os.environ['DROPBOX_ACCESS_TOKEN']
 
-# AWS Config
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+
